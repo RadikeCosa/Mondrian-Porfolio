972bb5d053ff4764ffcc3a5336612dc207c24d31
Ramiro Cosa <ramirocosa@gmail.com>
2023-01-09 12:34:32 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { ProjectsPageComponent } from './components/pages/projects-page/projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsPageComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
+import { ProjectsPageComponent } from './components/pages/projects-page/projects-page.component';
+import { HeaderComponent } from './components/commons/header/header.component';
+import { FooterComponent } from './components/commons/footer/footer.component';
+import { RightBarComponent } from './components/commons/right-bar/right-bar.component';
+import { LeftBarComponent } from './components/commons/left-bar/left-bar.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ProjectsPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    RightBarComponent,
+    LeftBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
